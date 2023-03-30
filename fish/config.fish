c71196faff4fecb80caf57379ec6cabcf4b7b0ed
Leonardo Vieira <vieira.leonardo@ifood.com.br>
2023-03-30 16:27:40 -0300

    feat: resolve conflict

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 fish_add_path /opt/homebrew/bin
-fish_add_path /Users/leonardovee/.cargo/bin 
-fish_add_path /Users/leonardovee/.local/bin
+fish_add_path /Users/$USER/.cargo/bin 
+fish_add_path /Users/$USER/.local/bin
 fish_add_path /usr/local/bin 
-fish_add_path /Users/leonardovee/go/bin/
+fish_add_path /Users/$USER/go/bin/
+fish_add_path /Users/$USER/Library/Python/3.10/bin
+
 function nvm
-      bass source ~/.nvm/nvm.sh ';' nvm $argv
+  bass source ~/.nvm/nvm.sh ';' nvm $argv
 end
