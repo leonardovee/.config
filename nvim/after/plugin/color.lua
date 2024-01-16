6e09be3f7f551e6a0f40463896b6a911d0b40f37
leonardovee <leoveeph@gmail.com>
2024-01-16 01:20:33 +0000

    colorscheme

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,2 +1,2 @@
 vim.opt.termguicolors = true
-vim.cmd.colorscheme 'melange'
+vim.cmd.colorscheme 'rose-pine'
--- nvim/lua/plugins/melange.lua
+++ nvim/lua/plugins/melange.lua
@@ -1,3 +1,11 @@
 return {
-    {"savq/melange-nvim"}
+    {
+        "rose-pine/neovim",
+        config = function()
+            local rosepine = require("rose-pine")
+            rosepine.setup({
+                disable_background = true
+            })
+        end,
+    }
 }
