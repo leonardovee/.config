cc43031df34940900542465a47f9701a0152c056
Leonardo Vieira <vieira.leonardo@ifood.com.br>
2024-04-29 14:16:03 -0300

    wtf just removed a plugin

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ function nvm
   bass source ~/.nvm/nvm.sh ';' nvm $argv
 end
 
+function cat
+  bat --style=plain $argv
+end
+
 fish_add_path /opt/homebrew/bin
 fish_add_path /Users/$USER/go/bin
 fish_add_path /Users/$USER/.cargo/bin
