dddbcd99f392e587388dbb966ab120836e623428
leonardovee <leoveeph@gmail.com>
2025-05-10 20:36:59 +0000

    idk

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/bin
-fish_add_path /Users/$USER/Library/Python/3.9/bin
-fish_add_path /Users/$USER/Library/Python/3.10/bin
-fish_add_path /Users/$USER/.cargo/bin
-fish_add_path /Users/$USER/.asdf/shims
+fish_add_path /home/$USER/Library/Python/3.9/bin
+fish_add_path /home/$USER/Library/Python/3.10/bin
+fish_add_path /home/$USER/.cargo/bin
+fish_add_path /home/$USER/.asdf/shims
 
 set -x EDITOR nvim
 set -x VISUAL $EDITOR
@@ -11,4 +11,4 @@ set -U fish_greeting
 
 source /Users/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
-source /opt/homebrew/opt/asdf/share/fish/vendor_completions.d/asdf.fish
+source /home/leonardovee/.config/fish/completions/asdf.fish
