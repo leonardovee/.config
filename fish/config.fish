6f4965b5573734ec30f45f71cac4abd34caf33fb
Leonardo Vieira <vieira.leonardo@ifood.com.br>
2025-05-15 10:33:27 -0300

    hahaha

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,13 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/bin
-fish_add_path /home/$USER/Library/Python/3.9/bin
-fish_add_path /home/$USER/Library/Python/3.10/bin
-fish_add_path /home/$USER/.cargo/bin
-fish_add_path /home/$USER/.asdf/shims
+fish_add_path /Users/$USER/Library/Python/3.9/bin
+fish_add_path /Users/$USER/Library/Python/3.10/bin
+fish_add_path /Users/$USER/.cargo/bin
+fish_add_path /Users/$USER/.asdf/shims
 
 set -x EDITOR nvim
 set -x VISUAL $EDITOR
 set -U fish_greeting 
 
 source /Users/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-
-source /home/leonardovee/.config/fish/completions/asdf.fish
+source /Users/$USER/.config/fish/completions/asdf.fish
