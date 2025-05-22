7eef1594c9f0d7df86cdc5f405380e31cb46ecbf
leonardovee <leoveeph@gmail.com>
2025-05-22 13:29:10 -0300

    bumps

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,14 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/bin
-fish_add_path /Users/$USER/Library/Python/3.9/bin
-fish_add_path /Users/$USER/Library/Python/3.10/bin
-fish_add_path /Users/$USER/.cargo/bin
-fish_add_path /Users/$USER/.asdf/shims
+fish_add_path /home/$USER/Library/Python/3.9/bin
+fish_add_path /home/$USER/Library/Python/3.10/bin
+fish_add_path /home/$USER/.cargo/bin
+fish_add_path /home/$USER/.asdf/shims
+fish_add_path /home/$USER/.asdf/bin
 
 set -x EDITOR nvim
 set -x VISUAL $EDITOR
 set -U fish_greeting 
 
-source /Users/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-source /Users/$USER/.config/fish/completions/asdf.fish
+source /home/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source /home/$USER/.config/fish/completions/asdf.fish
