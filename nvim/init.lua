42e380252464e17a1b3ef71e8e63f462ec20fe04
leonardovee <leoveeph@gmail.com>
2025-07-28 11:47:57 -0300

    ?

--- nvim/init.lua
+++ nvim/init.lua
@@ -331,7 +331,7 @@ require("lazy").setup({
     },
 
     -- themes
-    -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
     {
         "briones-gabriel/darcula-solid.nvim",
         dependencies = {
@@ -343,5 +343,5 @@ require("lazy").setup({
 
 -- color.lua
 vim.opt.termguicolors = true
-vim.cmd.colorscheme("darcula-solid")
--- vim.o.background = "light"
+vim.cmd.colorscheme("gruvbox")
+vim.o.background = "light"
