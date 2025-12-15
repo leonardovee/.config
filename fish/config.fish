ca37f64a5d6497c6834a45facc503e011e847085
leonardovee <leonardoves@proton.me>
2025-12-15 12:23:15 -0300

    ?

--- fish/config.fish
+++ fish/config.fish
@@ -2,26 +2,20 @@ function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 
-fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/bin
-fish_add_path /Users/$USER/Library/Python/3.9/bin
-fish_add_path /Users/$USER/Library/Python/3.10/bin
-fish_add_path /Users/$USER/.cargo/bin
-fish_add_path /Users/$USER/.asdf/shims
-fish_add_path /Users/$USER/.local/bin
-fish_add_path /home/leonardovee/.npm/bin
+fish_add_path /usr/local/go/bin/
+fish_add_path /opt/homebrew/bin
+
+fish_add_path ~/go/bin/
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.npm/bin
+fish_add_path ~/.asdf/shims
+fish_add_path ~/.local/bin
+fish_add_path ~/Library/Python/3.9/bin
+fish_add_path ~/Library/Python/3.10/bin
 
 set -x EDITOR nvim
 set -x VISUAL $EDITOR
-set -U fish_greeting 
+set -U fish_greeting
 
-#source /Users/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-#source /Users/$USER/.config/fish/completions/asdf.fish
-
-fzf --fish | source
-
-source /Users/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-source /Users/$USER/.config/fish/completions/asdf.fish
-
-# uv
-fish_add_path "/Users/vieira.leonardo/.local/bin"
+source ~/Vulkan/1.4.328.1/setup-env.sh
